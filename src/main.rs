2f89b3900174bd9e4d4cc5bed95dd4ff306cbd8d
Juliette Cordor <grassojames5@gmail.com>
2022-05-12 12:57:52 +1000

    fixes

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,4 @@
-use std::path::PathBuf;
-
+use hex_literal::hex;
 use sha1::{Digest, Sha1};
 
 use crate::args::{Args, ShaVersion};
@@ -9,9 +8,13 @@ mod args;
 fn main() {
     let args = Args::parse();
 
-    // if args.sha_version == ShaVersion::Sha1 {
-    //     let hasher = Sha1::new();
-    // }
+    if args.sha_version == ShaVersion::Sha1 {
+        let mut hasher = Sha1::new();
+
+        hasher.update(args.file_contents.as_slice());
+
+        let out = hasher.finalize();
+    }
 
     println!("Hashing file: {}", args.file_path.display());
 }
